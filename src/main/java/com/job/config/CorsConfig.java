d58e6c6ff2c13e34bb4980adf693d936decfff6f
Imre <imre.geczy@yahoo.co.uk>
2024-05-18 20:17:03 +0100

    feature/7 add config to application.properties

--- src/main/java/com/job/config/CorsConfig.java
+++ src/main/java/com/job/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.job.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -8,13 +9,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
 
+	@Value("${my.api.host}")
+	private String apiHost;
+	
+	@Value("${my.api.endpoint}")
+	private String apiEndpoint;	
+	
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                registry.addMapping(apiEndpoint)
+                        .allowedOrigins(apiHost)
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
